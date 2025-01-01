6e6364759aebed98c05b08d8969205e376dca34b
Razvalnuy <megamg001@gmail.com>
2025-01-01 22:39:43 +0300

    chat connecting FireStore

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,12 +1,11 @@
 "use client"
 import ChatWindows from "@/components/shared/chatWindow"
-import React from "react"
 
 interface Props {
 	className?: string
 }
 
-const Chat: React.FC<Props> = ({ className }) => {
+const Chat = ({ className }: Props) => {
 	return (
 		<div className={className}>
 			<ChatWindows />
